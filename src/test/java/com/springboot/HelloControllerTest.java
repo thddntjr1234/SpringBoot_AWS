c65f32c0a140e5ecc44ba96c5155e5e47ab8b145
thddntjr1234 <thddntjr112233@gmail.com>
2022-12-26 20:59:32 +0900

    HelloResponseDto 추가 및 관련 테스트코드, 컨트롤러 메소드 추가

--- src/test/java/com/springboot/HelloControllerTest.java
+++ src/test/java/com/springboot/HelloControllerTest.java
@@ -8,9 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // 테스트 실행 시 error: package org.junit does not exist 오류가 발생하면서
 // junit 관련 import 오류가 발생하는데 IDEA 설정 Build - Gradle 옵션에서 InteliJ IDEA로 실행되도록 수정하면 된다.
@@ -25,8 +25,20 @@ public class HelloControllerTest {
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-
         // isOk() == Http status가 200이 아닌가, content().string("") string()내의 내용이 맞는가
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
