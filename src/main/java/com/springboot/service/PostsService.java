b6a38508df6d84dc9a8c174540d6613c949939e2
thddntjr1234 <thddntjr112233@gmail.com>
2022-12-27 18:33:00 +0900

    Post save기능 구현을 위한 Controller, Dto, Service 클래스와 테스트 코드 추가

---
+++ src/main/java/com/springboot/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.springboot.service;
+
+import com.springboot.domain.posts.PostsRepository;
+import com.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/springboot/web/PostsApiController.java
@@ -0,0 +1,21 @@
+package com.springboot.web;
+
+import com.springboot.domain.posts.Posts;
+import com.springboot.service.PostsService;
+import com.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+
+}
