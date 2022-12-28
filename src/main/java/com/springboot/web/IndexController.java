94b9e2ce5f4d7fab2823b6d3ecb81a81a52628a4
thddntjr1234 <thddntjr112233@gmail.com>
2022-12-28 21:44:06 +0900

    mustache index 파일 추가 및 컨트롤러, 테스트 코드 추가

---
+++ src/main/java/com/springboot/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    // mustache starter을 적용했기 떄문에 문자열을 반환할 때 / 앞의 경로는 src/main/resources/templates로 자동 지정되고
+    // 뒤의 경로는 return하는 문자열.mustache가 붙게 된다.
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
