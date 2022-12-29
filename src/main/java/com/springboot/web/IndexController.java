02cd90524beaba136a22ce275dbf456876852e27
thddntjr1234 <thddntjr112233@gmail.com>
2022-12-29 20:00:43 +0900

    Feat: 게시글 등록 기능 추가
    게시글 등록 화면과 기능을 추가함

--- src/main/java/com/springboot/web/IndexController.java
+++ src/main/java/com/springboot/web/IndexController.java
@@ -11,4 +11,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("posts/save")
+    public String postSave() {
+        return "posts-save";
+    }
 }
