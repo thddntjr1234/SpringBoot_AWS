ba54e899393f6724af9eedd1fb627e6416575c90
thddntjr1234 <thddntjr112233@gmail.com>
2022-12-30 21:07:34 +0900

    Feat: 게시글 수정/삭제 기능 추가
    게시글 수정 기능
      - mustache 파일과 IndexController에 수정 메서드를 추가
    게시글 삭제 기능
      - PostsApiController, PostsService에 삭제 메서드를 추가

--- src/main/java/com/springboot/service/PostsService.java
+++ src/main/java/com/springboot/service/PostsService.java
@@ -47,4 +47,11 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts); // JpaRepository에서 지원하는 메소드
+    }
 }
--- src/main/java/com/springboot/web/IndexController.java
+++ src/main/java/com/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.springboot.web;
 
 import com.springboot.service.PostsService;
+import com.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -22,5 +24,15 @@ public class IndexController {
     @GetMapping("posts/save")
     public String postSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        // 게시글을 클릭하면 findById(id)를 통해 게시물을 dto에 담고
+        PostsResponseDto dto = postsService.findById(id);
+        // 템플릿 엔진이 이를 표시할 수 있도록 model 객체에 추가해준 뒤
+        model.addAttribute("post", dto);
+        // 게시글 수정 페이지를 띄운다
+        return "posts-update";
     }
 }
