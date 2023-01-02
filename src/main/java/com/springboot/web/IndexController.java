533a1b755c5490271c3c1144f3665b2d702a81f7
thddntjr1234 <thddntjr112233@gmail.com>
2023-01-02 21:23:24 +0900

    Feat: IndexController의 model에 세션정보 저장기능을 추가
    CustomOAuth2UserService에서 세션에 저장한 유저 정보를 가져와 model에 등록해서
    메인 페이지에 출력하도록 index 메소드를 수정함

--- src/main/java/com/springboot/web/IndexController.java
+++ src/main/java/com/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.springboot.web;
 
+import com.springboot.config.auth.dto.SessionUser;
 import com.springboot.service.PostsService;
 import com.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     // mustache starter을 적용했기 떄문에 문자열을 반환할 때 / 앞의 경로는 src/main/resources/templates로 자동 지정되고
     // 뒤의 경로는 return하는 문자열.mustache가 붙게 된다.
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
     public String index(Model model) { // Model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다. index.mustache에게 posts를 전달함
         model.addAttribute("posts", postsService.findAllDesc());
+
+        // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser을 저장하므로
+        // 로그인 성공 시 httpSession.getAttribute("user")을 통해 user에 저장된 세션의 정보를 넣어줄 수 있다.
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) { // 세선에 저장된 값이 있을 때 model에 등록
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
