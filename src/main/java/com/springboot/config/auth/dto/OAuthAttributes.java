6a0377110d22a7843ffb41b8901e604defdd2535
thddntjr1234 <thddntjr112233@gmail.com>
2023-01-03 21:09:50 +0900

    Feat: 네이버 로그인 기능 추가

--- src/main/java/com/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,6 +27,9 @@ public class OAuthAttributes {
     // of() - OAuth2User에서 반환하는 사용자 정보가 Map형태이므로 값 하나하나를 변환해 줘야 한다
     public static OAuthAttributes of(String registrationId, String userNameAttributeName,
                                      Map<String, Object> attributes) {
+        if ("naver" .equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -39,6 +42,19 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
 
     public User toEntity() {
         return User.builder()
