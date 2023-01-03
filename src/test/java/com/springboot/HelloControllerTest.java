6fc397b101a1e703a68c0e4a6e0d1375e47b468a
thddntjr1234 <thddntjr112233@gmail.com>
2023-01-03 21:27:19 +0900

    Refactor: 기존 테스트에 시큐리티 적용이 가능하도록 코드 수정
    @WebMvcTest 어노테이션이 등록된 클래스의 경우 Filter 옵션으로 SecurityConfig를 스캔 대상에서 제거하고
    적용되지 않은 클래스의 경우 MockMvc 인스턴스를 생성하고 @WithMockUser 어노테이션으로 권한을 설정하여 테스트 메소드를 실행할 때 Spring Security로 인한 미인증 사용자 문제를 해결하였음

--- src/test/java/com/springboot/HelloControllerTest.java
+++ src/test/java/com/springboot/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.springboot;
 
+import com.springboot.config.auth.SecurityConfig;
 import com.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,13 +19,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // 테스트 실행 시 error: package org.junit does not exist 오류가 발생하면서
 // junit 관련 import 오류가 발생하는데 IDEA 설정 Build - Gradle 옵션에서 InteliJ IDEA로 실행되도록 수정하면 된다.
 @RunWith(SpringRunner.class) // 테스트를 진행할 떄 JUnit4에 내장된 실행자 외에 다른 실행자를 실행시킨다(SpringRunner.class가 대상, 연결자 역할)
-@WebMvcTest(controllers = HelloController.class) // web(spring mvc)중 컨트롤러에 집중할 수 있는 어노테이션, @Controller, @ControllerAdvice 등을 사용가능
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+}) // web(spring mvc)중 컨트롤러에 집중할 수 있는 어노테이션, @Controller, @ControllerAdvice 등을 사용가능
 public class HelloControllerTest {
 
     @Autowired // 스프링이 관리하는 빈을 주입받음
     private MockMvc mvc; // 웹API를 테스트할 떄 사용(JUnit4)
 
     @Test
+    @WithMockUser(roles="USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -30,6 +37,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
