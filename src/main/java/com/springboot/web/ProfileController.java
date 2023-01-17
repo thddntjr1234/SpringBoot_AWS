67fea1fa288bb8d898ad30668937cc8d94b4d566
thddntjr1234 <thddntjr112233@gmail.com>
2023-01-17 17:05:40 +0900

    Chore: profile()메서드 호출 시 실제로 가져온 ActiveProfiles를 확인하기 위한 로그 메세지 추가

--- src/main/java/com/springboot/web/ProfileController.java
+++ src/main/java/com/springboot/web/ProfileController.java
@@ -18,7 +18,7 @@ public class ProfileController {
         List<String> profiles = Arrays.asList(env.getActiveProfiles()); // 실행 중인 ActiveProfile을 모두 가져온다(ec2 인스턴스에 있는 properties를 의미
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
         String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
-
+        System.out.println("profile() 메서드를 통해 들어간 profile은 " + profiles.toString());
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
